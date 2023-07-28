c7a512c42128f7c4cd55e05b4b0eb343dd88e42c
Documenter.jl <documenter@juliadocs.github.io>
2023-07-28 10:33:31 +0000

    build based on b2ad70c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Decay","category":"page"},{"location":"#Decay","page":"Home","title":"Decay","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Decay.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Decay]","category":"page"}]
+}
