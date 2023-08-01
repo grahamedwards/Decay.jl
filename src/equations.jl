e50aee34393e3072ba802634ac6db93ed47f95c0
Graham H. Edwards <grahamhedwards92@gmail.com>
2023-08-01 09:13:37 -0400

    touch up equations and add tests, add early drafts of series decay

--- src/equations.jl
+++ src/equations.jl
@@ -1,3 +1,5 @@
+## Direct Decay Equations: N, D, D_
+
 """
 
 ```julia
@@ -21,14 +23,16 @@ N(t::Number,λ::Number;No=1.) = No*exp(-λ*t)
 D(t, λ; N=1, Do=0)
 ```
 
-The classic age equation. Calculates the abundance of stable radiogenic daughter isotope `D` after time `t`, given the MODERN abundance of parent isotope `N` (default = 1) and its decay constant `λ`.  Optionally takes an initial daughter abundance `Dₒ` (default = 0).
+The classic age equation. 
+
+Calculates the abundance of stable radiogenic daughter isotope `D` after time `t`, given the MODERN abundance of parent isotope `N` (default = 1) and its decay constant `λ`.  Optionally takes an initial daughter abundance `Dₒ` (default = 0).
 
 Equation: `D = Dₒ + N ( exp(-λ t) - 1 )`
 
 see also: `D_`
 
 """
-D(t::Number, λ::Number; N=1., Do=0.) = Do + N *(exp(-λ*t)-1)
+D(t::Number, λ::Number; N=1., Do=0.) = Do + N *(exp(λ*t)-1)
 
 
 
@@ -48,7 +52,47 @@ Equation: `D = Dₒ +  Nₒ ( 1 - exp(-λ t) )`
 see also: `D`
 
 """
-D_(t::Number, λ::Number; No=1., Do=0.) = Do + No * (1 - exp(-λ*t))
+D_(t::Number, λ::Number; No::Number=1., Do::Number=0.) = Do + No * (1 - exp(-λ*t))
+
+
+
+
+
+##--------------------------------------------------
+
+## Series Decay Equations
+
+# Probably both overspecific...
+# Would rather 
+# AU48(t,AU48i;λ234=2.8234e-6)= 1 + (AU48i - 1) * exp(-λ234*t)
+# function dAdt(t::Int,Ao,f,λ234=2.8234e-6)
+#           A = Ao
+#           for i in 1:t
+#              A = 1 + (A - 1) * exp(-λ234) + f
+#           end   
+#            A
+#        end
+
+
+
+
+
+
+##--------------------------------------------------
+
+## Converting from half-life to decay constant and back again, a nucleus' tale...
+
+"""
+
+```julia
+lambda(halflife)
+```
+Calculates the decay constant (λ) from a half-life (`t`) with ` = log(2)/t`
+
+see also: `halflife`
+
+"""
+lambda(halflife::Number) = 0.6931471805599453/float(halflife)
 
 
 
@@ -64,20 +108,4 @@ Calculates the half-life from a decay constant (λ) with ` log(2)/λ`
 see also: `lambda`
 
 """
-halflife(lambda::Number) = 0.6931471805599453/float(lambda)
-
-
-
-
-
-"""
-
-```julia
-lambda(halflife)
-```
-Calculates the decay constant (λ) from a half-life (`t`) with ` = log(2)/t`
-
-see also: `halflife`
-
-"""
-lambda(halflife::Number) = 0.6931471805599453/float(halflife)
+halflife(lambda::Number) = Decay.lambda(lambda)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,21 @@ using Decay
 using Test
 
 @testset "Decay.jl" begin
-    # Write your tests here.
+
+# The basics
+    @test Decay.halflife(5) ≈ Decay.lambda(5.) ≈ log(2.)/5.
+    
+    @test λ.U238 ≈ 1.551254796141587e-10
+
+# Direct decay equations 
+    @test Decay.N(Decay.halflife(λ.U238),λ.U238) ≈ 0.5
+    
+    @test Decay.D(Decay.halflife(λ.U238),λ.U238,N=0.5) ≈ 0.5
+    @test Decay.D(Decay.halflife(λ.U238),λ.U238,N=0.5,Do=1) ≈ 1.5
+
+    @test Decay.D_(2*Decay.halflife(λ.U238),λ.U238,No=1) ≈ 0.75
+    @test Decay.D_(2*Decay.halflife(λ.U238),λ.U238,No=1,Do=1) ≈ 1.75
+
+# Series decay equations
+
 end
